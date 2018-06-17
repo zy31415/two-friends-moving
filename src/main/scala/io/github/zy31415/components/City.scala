bb60381653418b85eb72c265a5696149d8e5ee90
Yang Zhang <zy31415@gmail.com>
2018-06-18 01:41:48 +0800

    Implement input reader.

--- src/main/scala/io/github/zy31415/components/City.scala
+++ src/main/scala/io/github/zy31415/components/City.scala
@@ -3,30 +3,89 @@ package io.github.zy31415.components
 import java.io.{BufferedReader, Reader}
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
-class City(val name: String, val pos: (Double, Double)){
-  val neighbors = mutable.HashMap[Int, Double]()
+class City(val name: String){
+  var pos: (Double, Double) = _
+  val neighbors = mutable.HashMap[City, Double]()
+
+  override def hashCode() = name.hashCode
+
+  override def equals(other : Any) : Boolean = other match {
+    case that : City => this.name == that.name
+    case _ => false
+  }
+
+  def addNeighbor(neighbor: City): Unit =
+    if (!neighbors.contains(neighbor)) {
+      neighbors += (neighbor -> distance(neighbor))
+    }
+
+  def distance(city: City): Double = {
+    val d1 = pos._1 - city.pos._1
+    val d2 = pos._2 - city.pos._2
+    math.sqrt(d1*d1 + d2 * d2)
+  }
 }
 
 object City {
   var cities = new ArrayBuffer[City]()
 
+  // TODO: Refactor this code
   def readInput(input: Reader): Unit = {
-    val reader = new BufferedReader(input)
+    val (counter, neighborNamesList) = createCites(input)
+    makeNeighborConnections(counter, neighborNamesList)
+  }
 
+  private def createCites(input: Reader): (mutable.HashMap[String, City], ArrayBuffer[ArrayBuffer[String]]) = {
+    var counter = new mutable.HashMap[String, City]()
+
+    val reader = new BufferedReader(input)
     var line = reader.readLine()
 
-    // TODO: parse strings to generate cities.
+    val neighborNamesList: ArrayBuffer[ArrayBuffer[String]] = ArrayBuffer()
+
     while (line != null) {
       val words = line.trim.split(",")
-      if (words.size > 1) {
-        val name = words(0);
+      if (words.size >= 3) {
+        val name = words(0).trim
         val pos = (words(1).toDouble, words(2).toDouble)
-        val city = new City(name, pos)
+
+        val city = new City(name)
+        counter += (city.name -> city)
+        // set position and add to the city array
+        city.pos = pos
         cities.append(city)
+
+        neighborNamesList.append(ArrayBuffer())
+
+        // add neighbours to make connections
+        var i = 3
+        while (i < words.size) {
+          neighborNamesList.last.append(words(i).trim)
+          i += 1
+        }
+      } else if (words.size > 1) {
+        throw new IllegalArgumentException("Input file has wrong format.")
       }
       line = reader.readLine()
+    }
+    (counter, neighborNamesList)
+  }
+
+  private def makeNeighborConnections(counter: mutable.HashMap[String, City],
+                                      neighborNamesList: ArrayBuffer[ArrayBuffer[String]]): Unit = {
+    // setup neighbour
+    var i = 0
+    for (neighborNames <- neighborNamesList) {
+
+      val city = cities(i)
+
+      for (neighborName <- neighborNames) {
+        val neighbor = counter(neighborName)
+        city.neighbors += (neighbor -> city.distance(neighbor))
+      }
+      i += 1
     }
   }
 }
